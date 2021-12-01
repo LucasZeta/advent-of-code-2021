d5adc64daeefcfae8f6e79832ce0fee847e30a00
Lucas Conceição <lucas.conceicao@thoughtworks.com>
2021-12-01 07:41:59 -0300

    Day 1 part 2

--- src/main/kotlin/com/lucaszeta/adventofcode2021/day1/day1.kt
+++ src/main/kotlin/com/lucaszeta/adventofcode2021/day1/day1.kt
@@ -14,10 +14,28 @@ fun countDepthIncreases(depthMeasurements: List<Int>): Int {
     return timesDepthIncreased
 }
 
+fun countThreeMeasurementIncreases(depthMeasurements: List<Int>): Int {
+    var timesDepthIncreased = 0
+
+    depthMeasurements.forEachIndexed { index, currentDepth ->
+        if (index < 3) return@forEachIndexed
+
+        val currentMeasurement = currentDepth + depthMeasurements[index - 1] + depthMeasurements[index - 2]
+        val lastMeasurement = depthMeasurements[index - 1] + depthMeasurements[index - 2] + depthMeasurements[index - 3]
+
+        if (currentMeasurement > lastMeasurement) {
+            timesDepthIncreased++
+        }
+    }
+
+    return timesDepthIncreased
+}
+
 fun main() {
     val input = getResourceAsText("/day1/sweep-report.txt")
         .split("\n")
         .map { it.toInt() }
 
     println("Times measurements increased: ${countDepthIncreases(input)}")
+    println("Times a 3-measurement window increased: ${countThreeMeasurementIncreases(input)}")
 }
--- src/test/kotlin/com/lucaszeta/adventofcode2021/day1/Day1KtTest.kt
+++ src/test/kotlin/com/lucaszeta/adventofcode2021/day1/Day1KtTest.kt
@@ -1,37 +1,89 @@
 package com.lucaszeta.adventofcode2021.day1
 
 import com.google.common.truth.Truth.assertThat
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 
 class Day1KtTest {
 
-    @Test
-    fun `should count increases in depth`() {
-        val output = countDepthIncreases(listOf(
-            199,
-            200,
-            208,
-            210,
-            200,
-            207,
-            240,
-            269,
-            260,
-            263
-        ))
+    @Nested
+    inner class CountDepthIncreases {
 
-        assertThat(output).isEqualTo(7)
+        @Test
+        fun `should count increases in depth`() {
+            val output = countDepthIncreases(
+                listOf(
+                    199,
+                    200,
+                    208,
+                    210,
+                    200,
+                    207,
+                    240,
+                    269,
+                    260,
+                    263
+                )
+            )
+
+            assertThat(output).isEqualTo(7)
+        }
+
+        @Test
+        fun `should return 0 when there are no increases`() {
+            val output = countDepthIncreases(
+                listOf(
+                    210,
+                    208,
+                    200,
+                    199
+                )
+            )
+
+            assertThat(output).isEqualTo(0)
+        }
     }
 
-    @Test
-    fun `should return 0 when there are no increases`() {
-        val output = countDepthIncreases(listOf(
-            210,
-            208,
-            200,
-            199
-        ))
+    @Nested
+    @DisplayName("Count 3-measurement increases")
+    inner class CountThreeMeasurementIncreases {
 
-        assertThat(output).isEqualTo(0)
+        @Test
+        fun `should count increases in groups of 3`() {
+            val output = countThreeMeasurementIncreases(
+                listOf(
+                    199,
+                    200,
+                    208,
+                    210,
+                    200,
+                    207,
+                    240,
+                    269,
+                    260,
+                    263
+                )
+            )
+
+            assertThat(output).isEqualTo(5)
+        }
+
+        @Test
+        fun `should return 0 when there are no increases`() {
+            val output = countThreeMeasurementIncreases(
+                listOf(
+                    210,
+                    208,
+                    200,
+                    199,
+                    198,
+                    190,
+                    189,
+                )
+            )
+
+            assertThat(output).isEqualTo(0)
+        }
     }
 }
