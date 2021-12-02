8ef7fae23c91104df6938ae21d89e67944229097
Lucas Conceição <lucas.conceicao@thoughtworks.com>
2021-12-02 09:31:15 -0300

    Simplify enum class by removing parameter not needed

--- src/main/kotlin/com/lucaszeta/adventofcode2021/day2/Command.kt
+++ src/main/kotlin/com/lucaszeta/adventofcode2021/day2/Command.kt
@@ -1,14 +1,14 @@
 package com.lucaszeta.adventofcode2021.day2
 
-enum class Command(val input: String) {
-    UP("up"),
-    DOWN("down"),
-    FORWARD("forward");
+enum class Command {
+    UP,
+    DOWN,
+    FORWARD;
 
     companion object {
 
         fun fromInput(search: String) = values()
-            .firstOrNull { it.input == search }
+            .firstOrNull { it.name.lowercase() == search }
             ?: throw IllegalArgumentException("Command not found")
     }
 }
