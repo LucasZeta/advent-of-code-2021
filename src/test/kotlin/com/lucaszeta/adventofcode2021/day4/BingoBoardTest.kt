275be602b98fdcf2833f24daf818db2a9aae14f8
Lucas Conceição <lucas.conceicao@thoughtworks.com>
2021-12-04 11:29:41 -0300

    Fix lint

--- src/test/kotlin/com/lucaszeta/adventofcode2021/day4/BingoBoardTest.kt
+++ src/test/kotlin/com/lucaszeta/adventofcode2021/day4/BingoBoardTest.kt
@@ -7,13 +7,15 @@ class BingoBoardTest {
 
     @Test
     fun `should win when scored numbers form a row`() {
-        val board = BingoBoard(listOf(
-            listOf(22, 13, 17, 11, 0),
-            listOf(8, 2, 23, 4, 24),
-            listOf(21, 9, 14, 16, 7),
-            listOf(6, 10, 3, 18, 5),
-            listOf(1, 12, 20, 15, 19)
-        )).apply {
+        val board = BingoBoard(
+            listOf(
+                listOf(22, 13, 17, 11, 0),
+                listOf(8, 2, 23, 4, 24),
+                listOf(21, 9, 14, 16, 7),
+                listOf(6, 10, 3, 18, 5),
+                listOf(1, 12, 20, 15, 19)
+            )
+        ).apply {
             update(8)
             update(2)
             update(23)
@@ -29,13 +31,15 @@ class BingoBoardTest {
 
     @Test
     fun `should win when scored numbers form a column`() {
-        val board = BingoBoard(listOf(
-            listOf(22, 13, 17, 11, 0),
-            listOf(8, 2, 23, 4, 24),
-            listOf(21, 9, 14, 16, 7),
-            listOf(6, 10, 3, 18, 5),
-            listOf(1, 12, 20, 15, 19)
-        )).apply {
+        val board = BingoBoard(
+            listOf(
+                listOf(22, 13, 17, 11, 0),
+                listOf(8, 2, 23, 4, 24),
+                listOf(21, 9, 14, 16, 7),
+                listOf(6, 10, 3, 18, 5),
+                listOf(1, 12, 20, 15, 19)
+            )
+        ).apply {
             update(13)
             update(2)
             update(9)
@@ -51,13 +55,15 @@ class BingoBoardTest {
 
     @Test
     fun `should calculate score based on last scored number and unmarked numbers`() {
-        val board = BingoBoard(listOf(
-            listOf(14, 21, 17, 24, 4),
-            listOf(10, 16, 15, 9, 19),
-            listOf(18, 8, 23, 26, 20),
-            listOf(22, 11, 13, 6, 5),
-            listOf(2, 0, 12, 3, 7),
-        )).apply {
+        val board = BingoBoard(
+            listOf(
+                listOf(14, 21, 17, 24, 4),
+                listOf(10, 16, 15, 9, 19),
+                listOf(18, 8, 23, 26, 20),
+                listOf(22, 11, 13, 6, 5),
+                listOf(2, 0, 12, 3, 7),
+            )
+        ).apply {
             listOf(7, 4, 9, 5, 11, 17, 23, 2, 0, 14, 21, 24)
                 .forEach(::update)
         }
